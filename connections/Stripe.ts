4887d2bf7f8b6abc057a7a6700f666a508dd4919
Dougal Rea <dougal.rea@gmail.com>
2021-06-13 13:15:31 +0100

    working on authorization method

--- connections/Stripe.ts
+++ connections/Stripe.ts
@@ -12,6 +12,8 @@ import {
 
 import HttpClient from '../common/HTTPClient';
 
+const stripe = require('stripe')('sk_test_51J1pPKK6CA6lGmFjNFXl2rau2psaMEEvbP7qaGpqjYYApBreKt1wmuKVmMyRxZNHuuulmDAv4ypnvpvdXFawmYY500Hsp6liRr');
+
 const StripeConnection: ProcessorConnection<APIKeyCredentials, CardDetails> = {
   name: 'STRIPE',
 
@@ -30,7 +32,16 @@ const StripeConnection: ProcessorConnection<APIKeyCredentials, CardDetails> = {
   authorize(
     request: RawAuthorizationRequest<APIKeyCredentials, CardDetails>,
   ): Promise<ParsedAuthorizationResponse> {
-      throw new Error('Method Not Implemented');
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: request.amount,
+      currency: request.currencyCode,
+      payment_method: request.paymentMethod,
+      processorConfig = request.processorConfig
+    });
+    HttpClient
+      .request(url: '')
+    return
+    // throw new Error('Method Not Implemented');
   },
 
   /**
